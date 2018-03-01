3feac33776250c0cffb61ac2327f7b138513524b
iBzg <isa.baezg@gmail.com>
2018-02-28 23:02:14 -0600

    1er com

---
+++ registro/index.php
@@ -0,0 +1,18 @@
+!DOCTYPE html>
+<html>
+<head>
+<title>Bienvenida</title>
+<link href="css/styles.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+<h2>Home</h2>
+<br/>
+<p>mision</p>
+</br>
+<p>Visión</p>
+</br>
+<p>Valores</p>
+
+
+</html> 
---
+++ registro/registro.php
@@ -0,0 +1,35 @@
+!DOCTYPE html> 
+<html> 
+<head> 
+<title>Registro de usuarios</title> 
+<link href="/css/styles.css" rel="stylesheet" type="text/css">
+</head> 
+<body> 
+ 
+<h2>Registro de usuarios</h2> 
+<form action="/my-handling-form-page" method="post">
+<p>Ingrese la siguiente información: </p>
+    <div>
+        <label for="name">Nombre:</label>
+        <input type="text" id="name" />
+    </div>
+    <div>
+        <label for="appat">Apellido paterno:</label>
+        <input type="text" id="appat" />
+    </div>
+    <div>
+        <label for="apmat">Apellido materno:</label>
+        <input type="text" id="apmat" />
+    </div>
+    <div>
+        <label for="tel">Teléfono:</label>
+        <input type="text" id="tel"></label>
+    </div>
+    <div>
+        <label for="mail">Correo:</label>
+        <input type="email" id="mail" />
+    </div>
+</form>
+ 
+</html> 
+
